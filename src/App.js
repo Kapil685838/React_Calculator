15ebaac5e73c8ce1ac65e58d07073f20e78052f1
Kapil Chaudhary <kc685838@gmail.com>
2023-05-13 13:35:37 +0530

    Project done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Calculator from './Components/Calculator.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Calculator />
     </div>
   );
 }
